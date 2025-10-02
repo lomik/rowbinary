560f20ed9c1a56990c8f88163fa17e712bea2566
Lomonosov Roman <r.lomonosov@gmail.com>
2025-10-02 20:13:26 +0300

    benchmark custom tuple

--- example/custom_tuple_test.go
+++ example/custom_tuple_test.go
@@ -9,3 +9,7 @@ import (
 func TestCustomTuple(t *testing.T) {
 	rowbinary.TestType(t, CustomTupleType, CustomTuple{ID: 42, Name: "test"}, "CREATE TEMPORARY TABLE tmp (value Tuple(id UInt32, name String)) ENGINE=Memory; INSERT INTO tmp VALUES ((42, 'test')); SELECT value FROM tmp")
 }
+
+func BenchmarkCustomTuple(b *testing.B) {
+	rowbinary.BenchmarkType(b, CustomTupleType, CustomTuple{ID: 42, Name: "test"})
+}
